89cae9bba699960d2b83c3cdfeca706a69bc5e60
JihoMac <jiho@Jihoui-MacBookPro.local>
2021-12-15 03:04:10 +0900

    2021/12/15

---
+++ 8회차/문제1_최대공약수&최소공배수.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+public class Main {
+	public static final Scanner scanner = new Scanner(System.in);
+
+	public static void testCase(int caseIndex) {
+		int num1 = scanner.nextInt();
+		int num2 = scanner.nextInt();
+
+		long gcd =  MathUtil.getGCD(num1, num2);
+		long lcm =  MathUtil.getLCM(num1, num2);
+
+		System.out.printf("Case #%d:\n", caseIndex);
+		System.out.printf("%d %d\n", gcd, lcm);
+	}
+
+	public static void main(String[] args) throws Exception {
+		int caseSize = scanner.nextInt();
+
+		for (int caseIndex = 1; caseIndex <= caseSize; caseIndex += 1) {
+			testCase(caseIndex);
+		}
+	}
+
+}
+
+class MathUtil{
+
+	static long getGCD(long a, long b) {
+		while(a % b != 0) {
+			long c = a % b;
+			a = b;
+			b = c;
+		}
+		return b;
+	}
+
+	static long getLCM(long a, long b) {
+		return a * b / getGCD(a, b);
+	}
+}
